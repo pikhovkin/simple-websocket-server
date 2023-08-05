47a7dce556d22db483ec9c3db72b6c87736d8063
Sergei Pikhovkin <pikhovkin@gmail.com>
2023-08-05 16:57:25 +0300

    Handle SSLWantReadError and SSLWantWriteErrors on recv and send

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple-websocket-server',
-    version='0.4.3',
+    version='0.4.4',
     author='Sergei Pikhovkin',
     author_email='s@pikhovkin.ru',
     packages=find_packages(exclude=['tests*']),
